0d329304992c701a68c15788a242025ea36ff4a8
ElenaLj <elenaljubijankic@gmail.com>
2021-12-10 17:05:05 +0100

    second commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img src="../assets/vue-banner.png" alt="Vue banner">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Hero"
+}
+</script>
+
+<style scoped>
+img {
+    width: 80%;
+    margin: 0 auto;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <!-- parte che conterrà tutta la mia parte hmtl -->
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+    //logica del componente: come informazione minima devo creare l'export del mio oggetto
+    export default {
+        name: "Title"
+    }
+</script>
+
+<style scoped>
+/* se scoped non devo preoccuparmi di eventuali sovrascrizioni */
+h1 {
+    color: #3fb883;
+}
+</style>
